c1da16cc1387605435478d8139a45d592b173e7d
hanhnguyen0405 <37283287+hanhnguyen0405@users.noreply.github.com>
2022-03-22 17:50:59 -0500

    cleanup

--- streamlit_dashboard/streamlit_app.py
+++ streamlit_dashboard/streamlit_app.py
@@ -4,7 +4,6 @@ from gsheetsdb import connect
 import pandas as pd
 import datetime
 
-pd.set_option('display.max_colwidth', -1)
 
 # Create a connection object.
 credentials = service_account.Credentials.from_service_account_info(
@@ -25,11 +24,6 @@ def get_data(sheet_url):
     df = pd.DataFrame(rows)
     return df
 
-def make_clickable(link):
-    # target _blank to open new window
-    # extract clickable text to display for your link
-    return f'<a target="_blank" href="{link}">link</a>'
-
 
 filtered_sheet = st.secrets["private_gsheets_url_filtered"]
 unfiltered_sheet = st.secrets["private_gsheets_url_unfiltered"]
@@ -46,9 +40,6 @@ if not is_rendered or clicked:
     st.markdown(f'#### Last refreshed: {datetime.datetime.now()}')
 
     df = get_data(filtered_sheet)
-    # df['url'] = df['url'].apply(make_clickable)
-    # df = df.to_html(escape=False)
-    # st.write(df, unsafe_allow_html=True)
 
     st.dataframe(df, 2000, 800)
     is_rendered = True
